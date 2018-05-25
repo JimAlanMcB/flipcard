7ec3d2706fcf0b29e2e261ccc1f1d8e3a5ed324b
Jim Mcbrayer <jimm@dogtopia.local>
2018-05-25 07:19:58 -0700

    adding version 0.01

--- js/app.js
+++ js/app.js
@@ -2,13 +2,52 @@ let choice = '';
 let choice2 = '';
 let gameStarted = false;
 let attempts = 0;
+let firstSelection;
+let secondSelection;
 
 starRatingAttempts = document.querySelector('.star-rating');
 
+
+
 restart = document.getElementById('restart');
+
+
 $('.start').on('click', function () {
     //start timer
     // reset all things
+    let cardArray = [];
+    cardArray = document.querySelectorAll('.card');
+    
+
+    let i = 0;
+
+    cardArray.forEach(e => {
+        let fillArray = (e) => {
+
+            
+            //num = Math.floor(Math.random() * Math.floor(2));
+            if (findRandom() % 2) { 
+                e.innerHTML = "X";
+            }
+            else {
+                e.innerHTML = "O";
+            }
+
+            i++;
+        };
+
+        fillArray(e);
+    });
+
+    function findRandom() {
+        
+       let num = Math.floor(Math.random() * Math.floor(cardArray.length));
+       
+       return num;
+    }
+
+    attempts = 0;
+    starRatingAttempts.innerHTML = attempts;
     gameStarted = true;
     console.log(gameStarted);
     console.log('game started');
@@ -17,22 +56,26 @@ $('.start').on('click', function () {
 // let o = card.innerHTML;
 
 $('.card').on('click', function () {
+
     if (gameStarted) {
         if (choice.length > 0) {
+            secondSelection = this;
             choice2 = this.innerHTML;
             console.log('Second Choice' + choice2)
-            checkWinner();
+            checkWinner(secondSelection, firstSelection);
         } else {
+            firstSelection = this;
             choice = this.innerHTML;
             console.log('First Choice' + choice);
-
+            return firstSelection;
         }
     }
 });
 
-function checkWinner() {
+function checkWinner(firstSelection, secondSelection) {
     if (choice === choice2) {
-        alert('You Win!');
+        firstSelection.innerHTML = '';
+        secondSelection.innerHTML = '';
         choice = '';
         choice2 = '';
     } else {
@@ -49,6 +92,8 @@ function checkWinner() {
 
 
 restart.addEventListener('click', () => {
+    attempts = 0;
+    starRatingAttempts.innerHTML = attempts;
     choice = '';
     choice2 = '';
 });
---
+++ js/test.js
@@ -0,0 +1,13 @@
+let arr = ['hey', 'hello', 'whats up', 2, 'whats happening', 20];
+
+
+
+for(let i = 0; i < arr.length; i++){
+
+
+
+num = arr.indexOf(i);
+
+console.log(num);
+
+}
