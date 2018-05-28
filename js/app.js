b1b8921362c76956a8e570791efa19e31da64325
JimAlanMcB <alancode@gmail.com>
2018-05-27 18:30:50 -0700

    Complete game functioning. Still need to adjust Star rating, and current bug of clicking too fast to cheat

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// TODO:: make sure x's and o's are even -- done
+// add imgs instead of letters -- done
+// edit css to make pretty
+// fix star rating to reflect attempts correctly not just after 2 misses. 
+// shuffle cards randomly
+
+
 let choice = '';
 let choice2 = '';
 let gameStarted = false;
@@ -30,21 +37,19 @@ function startGame() {
     attempts = 0;
     moves = 0;
     clearChoices();
-    currentMoves.innerHTML = 'Current Moves: ' + moves;
-    starRatingAttempts.innerHTML = 'Attempts: ' + attempts;
+    currentMoves.innerHTML = 'Correct Flips: ' + moves;
+    starRatingAttempts.innerHTML = 'Incorrect Flips: ' + attempts;
     gameStarted = true;
 }
 
-// $('.front').on('click', function (e){
-//     flipCards(e.target);
-// });
-
 $('.card').on('click', function (e) {
-    console.log(e.target);
+
 
     if (gameStarted) {
+
+
         if ($(e.target).css('transform').length > 10) {
-            
+
             $(e.target).css('transform', '');
             $(e.target).prev().css('transform', '');
         } else
@@ -52,12 +57,12 @@ $('.card').on('click', function (e) {
         if (choice.length > 0) {
             secondSelection = this.lastElementChild;
             choice2 = this.lastElementChild.innerHTML;
-            console.log('Second Choice' + choice2)
+            console.log('Second Choice ' + choice2)
             checkWinner(secondSelection, firstSelection);
         } else {
             firstSelection = this.lastElementChild;
             choice = this.lastElementChild.innerHTML;
-            console.log('First Choice' + choice);
+            console.log('First Choice ' + choice);
             return firstSelection;
         }
     } else {
@@ -73,16 +78,15 @@ function flipCards(e) {
 
 function checkWinner(firstSelection, secondSelection) {
     if (firstSelection == secondSelection) {
-        setTimeout(clearChoices, 1000);
+        clearChoices();
         return;
     }
     if (choice === choice2) {
-        firstSelection.innerHTML = '';
-        secondSelection.innerHTML = '';
-        console.log(firstSelection + 'first selection div');
-        setTimeout(clearChoices, 1000);
+        //setTimeout(clearSelections, 1000);
+        clearSelections();
+        clearChoices();
         moves += 1;
-        currentMoves.innerHTML = 'Current Moves: ' + moves;
+        currentMoves.innerHTML = 'Correct Flips: ' + moves;
         if (moves === cardArray.length / 2) {
             console.log('winner');
             alert('winner winner chicken dinner');
@@ -96,11 +100,20 @@ function checkWinner(firstSelection, secondSelection) {
 
     } else {
         attempts += 1;
+        choice = '';
+        choice2 = '';
         setTimeout(clearChoices, 1000);
-        starRatingAttempts.innerHTML = 'Attempts: ' + attempts;
+        starRatingAttempts.innerHTML = 'Incorrect Flips: ' + attempts;
         adjustStarRating(attempts);
     }
 
+}
+
+function clearSelections() {
+    $(firstSelection).prev().hide();
+    $(secondSelection).prev().hide();
+    firstSelection.innerHTML = '';
+    secondSelection.innerHTML = '';
 }
 
 function clearChoices() {
@@ -129,7 +142,6 @@ function fillStars() {
     }
 }
 
-
 function startTimer(duration, display) {
     running = true;
     let totalTimeView = document.querySelector('.timer');
@@ -148,8 +160,8 @@ function startTimer(duration, display) {
         if (minutes < 1) {
             totalTimeView.innerHTML = seconds;
         } else {
-            totalTimeView.innerHTML = minutes + ":" + seconds; //showTimeView
-        } //debugger;
+            totalTimeView.innerHTML = minutes + ":" + seconds;
+        }
 
         if (running == false) {
             clearTimer();
@@ -178,26 +190,53 @@ function checkTimer() {
 
 
 function randomizeCards() {
-    let i = 0;
+
+    $('.front').show();
+
+    let iconArray = ['&#xf1b9;', '&#xf206;', '&#xf0fb;', '&#xf1e3;', '&#xf0eb;', '&#xf1b0;', '&#xf135;', '&#xf2dc;']
+
+    let cardIconArray = [];
+    let schuffleArray = [];
 
     cardArray.forEach(e => {
-        let fillArray = (e) => {
-
-            if (findRandom() % 2) {
-                e.innerHTML = "X";
-            } else {
-                e.innerHTML = "O";
-            }
-            i++;
-        };
-
-        fillArray(e);
+        schuffleArray.push(e);
     });
 
+    schuffleArray.forEach(e => {
+
+
+        let fillArray = (e) => {
+
+            num = findRandom();
+            cardIconArray.push(iconArray[num]);
+            cardIconArray.push(iconArray[num]);
+
+        };
+        fillArray(e);
+
+    });
+    //Fisher-Yates Shuffle - bost.ocks.org/mike/shuffle
+    function shuffle(array) {
+        let m = array.length,
+            t, i;
+
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+    shuffle(schuffleArray);
+    for (let i = 0; i < schuffleArray.length; i++) {
+        schuffleArray[i].innerHTML = cardIconArray[i];
+    }
+
     function findRandom() {
-
-        let num = Math.floor(Math.random() * Math.floor(cardArray.length));
-
+        let num = Math.floor(Math.random() * Math.floor(iconArray.length));
         return num;
     }
+
 }
